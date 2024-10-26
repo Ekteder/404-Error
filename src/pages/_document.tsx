eeeadd799bd701197438ff338bd8c2f7264cb9fd
Ekteder <144257567+Ekteder@users.noreply.github.com>
2024-10-26 10:30:38 +0600

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <meta name="google-site-verification" content="gP5gGQwXgain37Oh_AZhWox-igJHBD55-3MvNjlAS9s" />
       <body>
         <Main />
         <NextScript />
