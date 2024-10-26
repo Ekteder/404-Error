4cb240e4acec607e1a046f4a38696f7db6ac3cb0
Ekteder <144257567+Ekteder@users.noreply.github.com>
2024-10-26 10:32:30 +0600

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript, DocumentProps } from 'next/document'
 
-export default function Document() {
+export default function Document(): JSX.Element {
   return (
     <Html lang="en">
-      <Head />
-      <meta name="google-site-verification" content="gP5gGQwXgain37Oh_AZhWox-igJHBD55-3MvNjlAS9s" />
+      <Head>
+        <meta name="google-site-verification" content="gP5gGQwXgain37Oh_AZhWox-igJHBD55-3MvNjlAS9s" />
+      </Head>
       <body>
         <Main />
         <NextScript />
