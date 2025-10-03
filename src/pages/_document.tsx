1fa21b5a687394229e98197d00d044e700121526
Ekteder <iktidarsanglap01@gmail.com>
2025-10-04 01:49:15 +0600

    new 404 page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document(): JSX.Element {
   return (
     <Html lang="en">
       <Head>
-        <meta name="google-site-verification" content="gP5gGQwXgain37Oh_AZhWox-igJHBD55-3MvNjlAS9s" />
+        <meta
+          name="google-site-verification"
+          content="gP5gGQwXgain37Oh_AZhWox-igJHBD55-3MvNjlAS9s"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
-
